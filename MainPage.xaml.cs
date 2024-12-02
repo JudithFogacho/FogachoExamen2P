29ea65d7b8046e6d582e311f2db00450555bcb51
JudithFogacho <judith.fogacho@udla.edu.ec>
2024-12-02 10:31:38 -0500

    Funcionalidad del botón en el MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,28 +1,45 @@
 ﻿using FogachoExamen2P.Models;
+using Microsoft.VisualBasic;
 
 namespace FogachoExamen2P
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        private readonly AFConversion conversion = new ();
         public MainPage()
         {
             InitializeComponent();
             //InitializePickers();
 
         }
-        /*private void InitializePickers() {
-            var conversiones = Enum.GetValues(typeof(AFTipoMoneda));
-            FromCurrencyPicker.ItemsSource = conversiones;
-            ToCurrencyPicker.ItemsSource = conversiones;
+        private void InitializePickers() {
+            var currencies = Enum.GetValues(typeof(AFTipoMoneda));
+            FromCurrencyPicker.ItemsSource = currencies;
+            ToCurrencyPicker.ItemsSource = currencies;
         }
 
         private void OnConvertClicked(object sender, EventArgs e)
         {
-            if(decimal)
+            if (decimal.TryParse(AmountEntry.Text, out decimal amount) && FromCurrencyPicker.SelectedItem is AFTipoMoneda fromCurrency
+                && ToCurrencyPicker.SelectedItem is AFTipoMoneda toCurrency)
+            {
+                try
+                {
+                    var result = conversion.Conversion(fromCurrency, toCurrency, amount);
+                    ResultLabel.Text = $"Resultado: {result:F2} {toCurrency}";
 
-        }*/
+                }
+                catch (Exception ex)
+                {
+                    ResultLabel.Text = $"Error: {ex.Message}";
+                }
+
+            }
+            else {
+                ResultLabel.Text = " Ingrese valores validos";
+            }
+
+        }
     }
 
 }
